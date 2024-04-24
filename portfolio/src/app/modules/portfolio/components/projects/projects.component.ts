2c77d0773b9f3ca08d83d0f11a0e0f4db560a7aa
Maylla Krislainy Matos de Sousa <47458443+maydMoon@users.noreply.github.com>
2024-04-24 15:01:38 -0300

    [DIALOG] Projects Component

--- portfolio/src/app/modules/portfolio/components/projects/projects.component.ts
+++ portfolio/src/app/modules/portfolio/components/projects/projects.component.ts
@@ -3,14 +3,25 @@ import { Component, inject, signal } from '@angular/core';
 // Interface
 import { IProjects } from '../../interface/IProjects.interface';
 
+// Material
+import { MatDialog, MatDialogModule } from '@angular/material/dialog';
+
+// Enum
+import { EDialogPanelClass } from '../../enum/EDialogPanelClass.enum';
+
+// Dialog
+import { DialogProjectsComponent } from '../dialog/dialog-projects/dialog-projects.component';
+
 @Component({
   selector: 'app-projects',
   standalone: true,
-  imports: [],
+  imports: [MatDialogModule],
   templateUrl: './projects.component.html',
   styleUrl: './projects.component.scss',
 })
 export class ProjectsComponent {
+  #dialog = inject(MatDialog);
+
   public arrayProjects = signal<IProjects[]>([
     {
       src: 'assets/img/projects/unb-web.png',
@@ -28,4 +39,11 @@ export class ProjectsComponent {
       ],
     },
   ]);
+
+  public openDialog(data: IProjects) {
+    this.#dialog.open(DialogProjectsComponent, {
+      data,
+      panelClass: EDialogPanelClass.PROJECTS,
+    });
+  }
 }
