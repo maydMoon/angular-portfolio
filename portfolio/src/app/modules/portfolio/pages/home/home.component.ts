60aa40f26fafae1f35e76649e3d80bb558afd4f2
Maylla Krislainy Matos de Sousa <mayllak@hotmail.com>
2024-04-24 10:28:39 -0300

    [HTML] Knowledge Component

--- portfolio/src/app/modules/portfolio/pages/home/home.component.ts
+++ portfolio/src/app/modules/portfolio/pages/home/home.component.ts
@@ -2,14 +2,13 @@ import { Component } from '@angular/core';
 
 // Components
 import { HeaderComponent } from '../../components/header/header.component';
+import { KnowledgeComponent } from '../../components/knowledge/knowledge.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [HeaderComponent],
+  imports: [HeaderComponent, KnowledgeComponent],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.scss'
+  styleUrl: './home.component.scss',
 })
-export class HomeComponent {
-
-}
+export class HomeComponent {}
