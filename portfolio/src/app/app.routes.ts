da4fa5c90ee119f8c590406567d3edde724bdd42
Maylla Krislainy Matos de Sousa <mayllak@hotmail.com>
2024-04-24 09:49:42 -0300

    Initial Configs
    - Projeto de portfolio

---
+++ portfolio/src/app/app.routes.ts
@@ -0,0 +1,11 @@
+import { Routes } from '@angular/router';
+
+//Components
+import { HomeComponent } from './modules/portfolio/pages/home/home.component';
+
+export const routes: Routes = [
+  {
+    path:'',
+    component: HomeComponent
+  }
+];
